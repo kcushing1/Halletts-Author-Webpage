abe9c652ef8d6057eabe892725d97a8ba7821ab3
Kasey Cushing <kasey.cushing@gmail.com>
2021-03-06 11:08:51 -0500

    flashfiction carousel loads storycontext data as cards

--- client/src/components/Carousel/MyCarousel.js
+++ client/src/components/Carousel/MyCarousel.js
@@ -1,27 +1,37 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import "./carousel.css";
 import Carousel from "react-elastic-carousel";
 import FictionCard from "../FictionCard";
+import StoryContext from "../../contexts/StoryContext";
 
-export default class App extends Component {
-  state = {
-    items: [
-      { id: 1, title: "item #1" },
-      { id: 2, title: "item #2" },
-      { id: 3, title: "item #3" },
-      { id: 4, title: "item #4" },
-      { id: 5, title: "item #5" },
-    ],
-  };
+export default function MyCarousel() {
+  //class App extends Component
+  // state = {
+  //   items: [
+  //     { id: 1, title: "item #1" },
+  //     { id: 2, title: "item #2" },
+  //     { id: 3, title: "item #3" },
+  //     { id: 4, title: "item #4" },
+  //     { id: 5, title: "item #5" },
+  //   ],
+  // };
+  const stories = useContext(StoryContext);
 
-  render() {
-    const { items } = this.state;
-    return (
-      <Carousel>
-        {items.map((item) => (
-          <div key={item.id}>{item.title}</div>
-        ))}
-      </Carousel>
-    );
-  }
+  // render() {
+  //const { items } = this.state;
+  return (
+    <Carousel>
+      {stories.map((story) => {
+        return (
+          <FictionCard
+            key={story.id}
+            title={story.title}
+            text={story.text}
+            src={story.image}
+          />
+        );
+      })}
+    </Carousel>
+  );
+  // }
 }
--- client/src/pages/visible/FlashFiction/FlashFiction.js
+++ client/src/pages/visible/FlashFiction/FlashFiction.js
@@ -4,23 +4,10 @@ import FictionCard from "../../../components/FictionCard";
 import Carousel from "../../../components/Carousel/MyCarousel";
 
 export default function FlashFiction() {
-  const stories = useContext(StoryContext);
-
   return (
     <div id="flashfiction">
       <p>flash fiction here</p>
-      <Carousel>
-        {stories.map((story) => {
-          return (
-            <FictionCard
-              key={story.id}
-              title={story.title}
-              text={story.text}
-              src={story.image}
-            />
-          );
-        })}
-      </Carousel>
+      <Carousel />
     </div>
   );
 }
