122a8f769bd500eb98556810d4f30919d8681ad1
Kasey Cushing <kasey.cushing@gmail.com>
2021-03-15 15:06:35 -0400

    deploy troubleshooting: reverse models cleanup, create .env.deployment file in /client

--- models/user.js
+++ models/user.js
@@ -3,6 +3,16 @@ const { triggerAsyncId } = require("async_hooks");
 const { Model } = require("sequelize");
 const bcrypt = require("bcryptjs");
 module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
   User.init(
     {
       username: {
