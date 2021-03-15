3466d4cbb6ee66d332cb307f445a2b72750211fa
Kasey Cushing <kasey.cushing@gmail.com>
2021-03-15 19:25:30 -0400

    delete unused images, modify seeders

--- seeders/20210303132141-demo-message.js
+++ seeders/20210303132141-demo-message.js
@@ -8,14 +8,16 @@ module.exports = {
         {
           name: "John Doe",
           email: "john@hi.com",
-          text: "this is test 1 john doe",
+          text:
+            "Such a fan! I love your Animal Crackers story and look forward to your future works!",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           name: "Jane Deer",
           email: "jane@hi.com",
-          text: "this is test 2 jane deer",
+          text:
+            "Hi Hallett! If you're interested in self-publishing, I manage a small publishing company that works with many authors just like you! I would love to connect and talk more!",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
